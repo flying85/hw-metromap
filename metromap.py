87e3f69d0164f1a579490a772878cf342685480b
Paolo-DiAchille <paolo.diachille@ibm.com>
2019-08-03 11:26:46 -0400

    Added tests

---
+++ metromap.py
@@ -0,0 +1,246 @@
+import os
+import urllib.request
+from urllib.parse import urljoin
+import json
+import sys
+
+class Stop:
+    '''Class to describe subway stops
+
+       Stop.parents -- set of routes intersecting at the stop
+    
+       Arguments:
+       sid -- subway stop id
+       name -- subway stop name
+    '''
+    def __init__(self, sid, name):
+        self.sid = sid
+        self.name = name
+        self.parents = set()
+        
+
+class Route:
+    '''Class to describe subway routes.
+
+       Route.stops -- dictionary of subway stops along route 
+    
+       Arguments: 
+       rid -- route id
+       long_name -- long name of the route
+       short_name -- short name of the route
+    '''
+    
+    def __init__(self, rid, long_name, short_name):
+        
+        self.rid = rid
+        self.long_name = long_name
+        self.short_name = short_name
+        self.stops = set()
+        self.nstops = 0
+        self.neighbors = set()
+
+    def __repr__(self):
+        return self.long_name
+        
+class Map:
+    '''Retrieves subway routes and stops 
+
+       Map.routes -- dictionary of all subway routes
+       Map.stops -- dictionary of all subway stops
+       Map.stops_by_name -- dictionary mapping stop names to stop ids
+
+       Arguments:
+       url -- root API url to retrieve subway routes and stops 
+    '''
+
+    def __init__(self, url='https://api-v3.mbta.com'):
+        self.url = url
+        self.routes = {}
+        self.stops = {}
+        self.stops_by_name = {}
+        self.routes_downloaded = False
+        self.stops_downloaded  = False
+
+    def get(self, path='routes', query_key='type', keys='0,1'):
+        '''Submits generic get requests to the API
+        
+           Arguments:
+           path -- main object to be downloaded (e.g., routes, stops)
+           query_key -- key for filtering
+           keys -- values for filtering
+
+           Returns:
+           message -- dictionary constructed from JSON API response
+        '''
+
+        url_path = [self.url, '%s?filter[%s]=%s' % (path, query_key, keys)]
+        url_path = '/'.join(url_path)
+        response = urllib.request.urlopen(url_path)
+        message = response.read().decode('utf-8')
+        message = json.loads(message)
+        return message
+
+    def get_routes(self, overwrite=False, query_key='type', keys='0,1'):
+        '''Retrieves subway routes from the API
+
+           Arguments:
+           overwrite -- flag to overwrite routes even if already
+                        downloaded
+           query_key -- key for filtering routes
+           keys -- values for filtering routes
+        '''
+        if overwrite or not self.routes_downloaded:
+            self.routes = {}
+            routes_dic = self.get(path='routes', 
+                                  query_key=query_key, 
+                                  keys=keys)
+            for route in routes_dic['data']:
+                self.routes[route['id']] = Route(rid=route['id'], 
+                                                 long_name=route['attributes']['long_name'],
+                                                 short_name=route['attributes']['short_name'])
+            self.routes_downloaded = True
+            self.stops_downloaded  = False
+
+    def get_stops(self, overwrite=False):
+        '''Retrieves subway stops from the API
+
+           Arguments:
+           overwrite -- flag to overwrite routes even if already downloaded
+        '''
+        if overwrite or not self.stops_downloaded:
+            for route in self.routes:
+                route_stops = self.get(path='stops', query_key='route', keys=route)
+                for route_stop in route_stops['data']:
+                    if route_stop['id'] not in self.stops:                        
+                        stop_name = route_stop['attributes']['name']
+                        self.stops[route_stop['id']] = Stop(sid=route_stop['id'],
+                                                            name=stop_name)
+                        self.stops_by_name[stop_name] = route_stop['id']
+                    self.stops[route_stop['id']].parents.add(route)
+                    self.routes[route].stops.add(route_stop['id'])
+                    self.routes[route].nstops += 1
+
+            self.stops_downloaded = True
+
+    
+    def max_stops(self):
+        '''Prints route with maximum number of subway stops
+        
+           Returns:
+           (route, nstops) -- route name, maximum number of stops
+        '''
+        self.get_routes()
+        self.get_stops()
+        max_stop_route = ''
+        max_nstops = -1
+        for route in self.routes:
+            if self.routes[route].nstops > max_nstops:                
+                max_nstops = self.routes[route].nstops
+                max_nstops_route = route
+
+        return (max_nstops_route, max_nstops)
+        
+    def min_stops(self):
+        '''Finds route with minimum number of subway stops
+
+           Returns:
+           (route, nstops) -- route name, maximum number of stops
+        '''
+        self.get_routes()
+        self.get_stops()
+        min_stop_route = ''
+        min_nstops = sys.maxsize
+        for route in self.routes:
+            if  self.routes[route].nstops < min_nstops:
+                min_nstops = self.routes[route].nstops
+                min_nstops_route = route
+
+        return (min_nstops_route, min_nstops)
+
+    def calc_route_adjacency(self):
+        '''Computes adjacency between routes and updates Route.neighbors and Stop.parents
+        '''
+        self.get_routes()
+        self.get_stops()
+        for stop in self.stops:
+            if len(self.stops[stop].parents) > 1 :
+                for parent in self.stops[stop].parents:
+                    other_parents = self.stops[stop].parents - {parent}
+                    self.routes[parent].neighbors.update(other_parents)            
+   
+
+    def trip_between_stops(self, src, dest):
+        '''Finds path between two stops
+        
+           Arguments:
+           src -- name of starting stop
+           dest -- name of destination stop
+
+           Returns:
+           trip -- list of metro routes from src to dest
+        '''
+        src_id  = self.stops_by_name[src]
+        dest_id = self.stops_by_name[dest]
+        src_routes = self.stops[src_id].parents
+        dest_routes = self.stops[dest_id].parents        
+        src_route = src_routes.pop()    
+        src_routes.add(src_route) 
+        dest_route = dest_routes.pop()    
+        dest_routes.add(dest_route) 
+        visited_routes = {src_route : 0}
+        visited_from   = {src_route : None}           
+        cur_routes = [src_route] 
+        i = 1
+        while cur_routes:            
+            next_routes = []
+            for cur_route in cur_routes:
+                for neighbor_route in self.routes[cur_route].neighbors:                    
+                    if neighbor_route not in visited_routes:
+                        visited_routes[neighbor_route] = i
+                        visited_from[neighbor_route] = cur_route
+                        next_routes.append(neighbor_route)
+            cur_routes = next_routes
+            i += 1
+
+        trip = [dest_route]
+        next_route = visited_from[dest_route]
+        while next_route:
+            trip = [next_route] + trip
+            next_route = visited_from[next_route]  
+        return trip
+                
+    def print_routes(self):
+        self.get_routes()
+        for route in self.routes:
+            print(self.routes[route])
+        
+    def print_connecting_stops(self):
+        self.get_routes()
+        self.get_stops()
+        for stop in self.stops:
+            if len(self.stops[stop].parents) > 1:
+                print(self.stops[stop].name + ' --> ' + ', '.join(self.stops[stop].parents))
+
+
+if __name__ == '__main__':
+
+    m = Map()
+    routes = m.get_routes()
+    print("======================================")
+    print("=========QUESTION 1===================")
+    print("======================================")
+    m.print_routes()
+    print("======================================")
+    print("=========QUESTION 2a==================")
+    print("======================================")
+    m.max_stops()
+    m.min_stops()
+    print("======================================")
+    print("=========QUESTION 2b==================")
+    print("======================================")
+    m.print_connecting_stops()
+    m.calc_route_adjacency()
+    print("======================================")
+    print("=========QUESTION 3===================")
+    print("======================================")
+    m.trip_between_stops('Brookline Village', 'Sullivan Square')
---
+++ tests.py
@@ -0,0 +1,85 @@
+import unittest
+import metromap
+
+class TestStop(unittest.TestCase):
+    def setUp(self):
+        self.stop = metromap.Stop('idx', 'test_stop')        
+
+    def test_attributes(self):
+        self.assertEqual(self.stop.sid, 'idx')
+        self.assertEqual(self.stop.name, 'test_stop')
+        self.assertFalse(self.stop.parents)
+
+
+class TestRoute(unittest.TestCase):
+    def setUp(self):
+        self.route = metromap.Route('idx', 'Long Route', 'Long')
+
+    def test_attributes(self):
+        self.assertEqual(self.route.rid, 'idx')
+        self.assertEqual(self.route.long_name, 'Long Route')
+        self.assertEqual(self.route.short_name, 'Long')
+        self.assertFalse(self.route.stops)
+        self.assertEqual(self.route.nstops, 0)
+        self.assertFalse(self.route.neighbors)
+        
+    def test_print(self):
+        self.assertEqual(repr(self.route), 'Long Route')
+
+class TestMap(unittest.TestCase):
+
+    def setUp(self):
+        self.m = metromap.Map()
+
+    def test_attributes(self):
+        self.assertEqual(self.m.url, 'https://api-v3.mbta.com')
+        self.assertFalse(self.m.routes)
+        self.assertFalse(self.m.stops)
+        self.assertFalse(self.m.stops_by_name)
+        self.assertFalse(self.m.routes_downloaded)
+        self.assertFalse(self.m.stops_downloaded)
+    
+    def test_get(self):
+        dd = self.m.get(path='routes', query_key='id', keys='Red,Green-B')
+        self.assertEqual(len(dd['data']), 2)
+
+    def test_get_routes(self):
+        self.m.get_routes(query_key='id', keys='Red,Green-B')
+        self.assertIn('Red', self.m.routes)
+        self.assertIn('Green-B', self.m.routes)        
+
+    def test_get_stops(self):
+        self.m.get_routes(query_key='id', keys='Red,Green-B')
+        self.m.get_stops()
+        self.assertIn('place-knncl', self.m.stops)
+        self.assertIn('place-knncl', self.m.routes['Red'].stops)
+        self.assertIn('place-harvd', self.m.stops)        
+        self.assertIn('place-harvd', self.m.routes['Green-B'].stops)        
+    def test_max_min_stops(self):
+        self.m.get_routes(query_key='id', keys='Red,Green-B')
+        self.m.get_stops()
+        self.assertEqual(self.m.max_stops()[0], 'Green-B')
+        self.assertEqual(self.m.min_stops()[0], 'Red')
+
+    def test_adjacency(self):
+        self.m.get_routes(query_key='id', keys='Red,Green-B')
+        self.m.get_stops()
+        self.m.calc_route_adjacency()
+        self.assertIn('Red', self.m.routes['Green-B'].neighbors)
+        self.assertIn('Green-B', self.m.routes['Red'].neighbors)
+
+    def test_trip(self):
+        self.m.get_routes(query_key='id', keys='Red,Green-B')
+        self.m.get_stops()
+        self.m.calc_route_adjacency()
+        trip = self.m.trip_between_stops('Kendall/MIT', 
+                                         'Harvard Avenue')
+        self.assertEqual(trip, ['Red', 'Green-B'])
+        trip = self.m.trip_between_stops('Harvard Avenue',
+                                         'Kendall/MIT')
+        self.assertEqual(trip, ['Green-B', 'Red'])
+
+
+if __name__ == '__main__':
+    unittest.main()
+     
